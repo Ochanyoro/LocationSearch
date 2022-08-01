bc0a9e48e294c114be1c707c935a83de345b89b8
Ochanyoro <wadaoo0223@icloud.com>
2022-08-01 16:39:42 +0900

    Initial Commit

---
+++ LocationSearch/LocationSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationSearchApp.swift
+//  LocationSearch
+//
+//  Created by 大和田一裕 on 2022/08/01.
+//
+
+import SwiftUI
+
+@main
+struct LocationSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
